c892db83c01f26e6e1912b360cddb7a5174d052c
inisik <38598886+inisik@users.noreply.github.com>
2018-04-22 17:54:18 -0400

    Update and rename ShakeSecurity.php to AbstractShakeSecurity.php

---
+++ src/AbstractShakeSecurity.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+/**
+ * Shake Autoloading.
+ * A better internet.
+ *
+ * @license <https://github.com/shake-php/autoloading/blob/master/LICENSE>.
+ * @link    <https://github.com/shake-php/autoloading>.
+ */
+
+namespace Shake;
+
+use RuntimeException;
+
+/**
+ * @class AbstractShakeSecurity.
+ */
+abstract class AbstractShakeSecurity
+{
+}
