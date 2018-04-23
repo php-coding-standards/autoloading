6813366899b85d73e7f239ca24be7e07e09247ad
inisik <38598886+inisik@users.noreply.github.com>
2018-04-23 10:56:11 -0400

    Update and rename AbstractShakeSecurity.php to AutoloaderInterface.php

---
+++ src/AutoloaderInterface.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+/**
+ * Shake Autoloading.
+ * Better php code equals better web applications.
+ *
+ * @license <https://github.com/shake-php/autoloading/blob/master/LICENSE>.
+ * @link    <https://github.com/shake-php/autoloading>.
+ */
+
+/**
+ * @interface AutoloaderInterface.
+ *
+ * The interface that defines each autoloader.
+ */
+interface AutoloaderInterface
+{
+}
