2d87bc12c3d2be38289bbbec19c25114fd79410c
inisik <inisik0820@gmail.com>
2018-04-25 01:06:59 -0400

    Add a reference function.

--- bin/files/test2.php
+++ bin/files/test2.php
@@ -7,3 +7,8 @@ declare(strict_types=1);
  * @license <https://github.com/shake-php/autoloading/blob/master/LICENSE>.
  * @link    <https://github.com/shake-php/autoloading>.
  */
+
+function Shake566648390099377666647733()
+{
+    return true;
+}
