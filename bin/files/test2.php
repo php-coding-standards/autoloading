a74a00db2b73deb3402e168081a72d1ac889d3b9
inisik <inisik0820@gmail.com>
2018-04-25 01:02:18 -0400

    Create test2.php

---
+++ bin/files/test2.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+/**
+ * Shake Autoloading.
+ * Better php code equals better web applications.
+ *
+ * @license <https://github.com/shake-php/autoloading/blob/master/LICENSE>.
+ * @link    <https://github.com/shake-php/autoloading>.
+ */
