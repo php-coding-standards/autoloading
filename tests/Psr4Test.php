8ab8ab7a035ffe0aa806dd6fa712d5d99e5934ba
inisik <inisik0820@gmail.com>
2018-04-24 22:28:30 -0400

    Add more code coverage.

--- tests/Psr4Test.php
+++ tests/Psr4Test.php
@@ -12,5 +12,21 @@ class Psr4Test extends PHPUnit\Framework\TestCase
         
         // Return test result.
         $this->assertTrue(true);
+        
+        // Psr4 Test Namespaces With Base Directories.
+        $namespaces = array(
+            'Inisik\\Psr4Test\\' => __DIR__ . '/../bin/psr4/1',
+            'Inisik\\Psr4TestTwo\\' => __DIR__ . '/../bin/psr4/2'
+        );
+        
+        // Try to register it with the new options configuration.
+        (new Psr4Autoloading)->register($namespaces);
+        
+        // Try to use a class.
+        $result = new Inisik\Psr4Test\Test;
+        $result = new Inisik\Psr4TestTwo\Test;
+
+        // Return test result.
+        $this->assertTrue(true);
     }
 }
