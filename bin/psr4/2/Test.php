891ac8df94066fc8094ce5d97247294ab344022e
inisik <inisik0820@gmail.com>
2018-04-24 22:38:53 -0400

    Add the secondary test psr4 namespace.

---
+++ bin/psr4/2/Test.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+/**
+ * Shake Autoloading.
+ * Better php code equals better web applications.
+ *
+ * @license <https://github.com/shake-php/autoloading/blob/master/LICENSE>.
+ * @link    <https://github.com/shake-php/autoloading>.
+ */
+
+namespace Inisik\Psr4TestTwo;
+
+/**
+ * @class Test
+ */
+class Test
+{
+}
