9ff9b32b8ad65cc59d102abf6c7462287a4144ea
inisik <inisik0820@gmail.com>
2018-04-24 22:32:10 -0400

    Add doctype header.

--- tests/Psr4Test.php
+++ tests/Psr4Test.php
@@ -1,4 +1,12 @@
 <?php
+declare(strict_types=1);
+/**
+ * Shake Autoloading.
+ * Better php code equals better web applications.
+ *
+ * @license <https://github.com/shake-php/autoloading/blob/master/LICENSE>.
+ * @link    <https://github.com/shake-php/autoloading>.
+ */
 
 class Psr4Test extends PHPUnit\Framework\TestCase
 {
