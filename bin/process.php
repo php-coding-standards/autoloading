fc98f0be245d9afe1b803b7ed2ae1032e6155b21
inisik <inisik0820@gmail.com>
2018-04-30 17:52:32 -0400

    Add some processing functions.

--- bin/process.php
+++ bin/process.php
@@ -1,1 +1,24 @@
+<?php
+declare(strict_types=1);
+/**
+ * Shake Autoloading.
+ * Better php code equals better web applications.
+ *
+ * @license <https://github.com/shake-php/autoloading/blob/master/LICENSE>.
+ * @link    <https://github.com/shake-php/autoloading>.
+ */
+
+namespace ShakeAutoloadingProcess239471002934583901294880124975124;
+
+function psr4(array $options = array()): void {
+    (new Psr4Autoloading)->register($options);
+}
+
+function files(array $options = array()): viod {
+    (new FilesAutoloading)->register($options);
+}
+
+function classmap(array $options = array()): void {
+    (new ClassmapAutoloading)->register($options);
+}
 
