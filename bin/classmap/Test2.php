7f3ed9f693b7166384d3403ebfeda15ed4047cbc
inisik <inisik0820@gmail.com>
2018-04-30 17:56:58 -0400

    Update and rename test2.php to Test2.php

---
+++ bin/classmap/Test2.php
@@ -0,0 +1,19 @@
+
+<?php
+declare(strict_types=1);
+/**
+ * Shake Autoloading.
+ * Better php code equals better web applications.
+ *
+ * @license <https://github.com/shake-php/autoloading/blob/master/LICENSE>.
+ * @link    <https://github.com/shake-php/autoloading>.
+ */
+
+namespace Inisik\ClassmapTest;
+
+/**
+ * @class Test2
+ */
+class Test2
+{
+}
