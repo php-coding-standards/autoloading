dd210ea32864c88cd91c01144534d45e8cb0797b
inisik <inisik0820@gmail.com>
2018-05-02 15:45:13 -0400

    Declare functions provided.

--- ext/shake_autoloading.c
+++ ext/shake_autoloading.c
@@ -5,3 +5,13 @@
 #include "php.h"
 #include "php_ini.h"
 #include "php_shake_autoloading.h"
+
+#define PHP_SHAKE_AUTOLOADING_VERSION "1.1.2"
+#define PHP_SHAKE_AUTOLOADING_EXTENSION_NAME "shake_autoloading"
+
+static function_entry hello_functions[] = {
+    PHP_FE(register_psr4_autoloader, NULL)
+    PHP_FE(register_files_autoloader, NULL)
+    PHP_FE(register_classmap_autoloader, NULL)
+    {NULL, NULL, NULL}
+};
