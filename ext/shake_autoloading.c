55fa713bd194e17ea18a3cbcdc4b8a7e30da9170
inisik <inisik0820@gmail.com>
2018-05-02 15:54:55 -0400

    Update shake_autoloading.c

--- ext/shake_autoloading.c
+++ ext/shake_autoloading.c
@@ -9,7 +9,14 @@
 #define PHP_SHAKE_AUTOLOADING_VERSION "1.1.2"
 #define PHP_SHAKE_AUTOLOADING_EXTENSION_NAME "shake_autoloading"
 
-static function_entry hello_functions[] = {
+extern zend_module_entry my_extension_module_entry;
+#define phpext_my_extension_ptr &my_extension_module_entry
+
+PHP_FUNCTION(register_psr4_autoloader);
+PHP_FUNCTION(register_files_autoloader);
+PHP_FUNCTION(register_classmap_autoloader);
+
+static function_entry shake_autoloading_functions[] = {
     PHP_FE(register_psr4_autoloader, NULL)
     PHP_FE(register_files_autoloader, NULL)
     PHP_FE(register_classmap_autoloader, NULL)
