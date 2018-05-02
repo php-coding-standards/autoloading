b038004e40762159392d3430dd7ff95d7ac4bd8c
inisik <inisik0820@gmail.com>
2018-05-01 20:12:38 -0400

    Allow function access.

--- bin/function_reference.php
+++ bin/function_reference.php
@@ -1,1 +1,21 @@
+<?php
+declare(strict_types=1);
+/**
+ * Shake Autoloading.
+ * Better php code equals better web applications.
+ *
+ * @license <https://github.com/shake-php/autoloading/blob/master/LICENSE>.
+ * @link    <https://github.com/shake-php/autoloading>.
+ */
 
+function psr4(array $options = array()): void {
+    (new \Psr4Autoloading)->register($options);
+}
+
+function files(array $options = array()): viod {
+    (new \FilesAutoloading)->register($options);
+}
+
+function classmap(array $options = array()): void {
+    (new \ClassmapAutoloading)->register($options);
+}
