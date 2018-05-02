56746f06aabe18b2990332967dce90ef8d030b33
inisik <inisik0820@gmail.com>
2018-05-02 12:20:49 -0400

    Update AutoloadManager.php

--- src/AutoloadManager.php
+++ src/AutoloadManager.php
@@ -1,1 +1,16 @@
+<?php
+declare(strict_types=1);
+/**
+ * Shake Autoloading.
+ * Better php code equals better web applications.
+ *
+ * @license <https://github.com/shake-php/autoloading/blob/master/LICENSE>.
+ * @link    <https://github.com/shake-php/autoloading>.
+ */
 
+/**
+ * @class AutoloadManager.
+ */
+class AutoloadManager implements AutoloadManagerInterface
+{
+}
