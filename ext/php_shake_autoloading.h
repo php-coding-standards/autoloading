6d8a196c74e0d2f94be1698f227eb1c91cd60578
inisik <inisik0820@gmail.com>
2018-05-04 01:32:16 -0400

    Update php_shake_autoloading.h

--- ext/php_shake_autoloading.h
+++ ext/php_shake_autoloading.h
@@ -1,1 +1,11 @@
+#ifndef PHP_SHAKE_AUTOLOADING_H
+#define PHP_SHAKE_AUTOLOADING_H 1
 
+extern zend_module_entry shake_autoloading_module_entry;
+#define phpext_shake_autoloading_ptr &shake_autoloading_module_entry
+
+#ifdef ZTS
+#include "TSRM.h"
+#endif
+
+#endif
